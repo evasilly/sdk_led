812eb857b53ae6595dff8e075ef6d568efbc0584
evasilly <apfelrobbe@gmail.com>
2020-03-16 15:33:50 +0300

    init

---
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.d
@@ -0,0 +1,74 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Core/Inc/stm32f4xx_hal_conf.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Include/core_cm4.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Include/core_cmSimd.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Core/Inc/stm32f4xx_hal_conf.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Include/core_cm4.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Include/core_cmSimd.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+D:/studies/new/VT/SEM3/ES/SDK_LED/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
